ef9f868dfe542ce39991711831d90c1e18f8623e
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-17 15:25:47 -0500

    initial push

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBBlBKhrUTu7HO7oA0TwRV996lDLiogd4k",
+    authDomain: "train-sched-cm.firebaseapp.com",
+    databaseURL: "https://train-sched-cm.firebaseio.com",
+    projectId: "train-sched-cm",
+    storageBucket: "",
+    messagingSenderId: "344397199148"
+};
+firebase.initializeApp(config);
