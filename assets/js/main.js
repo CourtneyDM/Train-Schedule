81388991fc80386dac35a0dd1c0617ca63aaf907
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-19 15:43:19 -0500

    running test to add results to page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 // Object to store train information
 var train = {};
-var currentTime, firstTime, minsTilTrain, nextTrain, tFrequency, tRemainder;
-
 
 // Initialize Firebase
 var config = {
@@ -17,7 +15,7 @@ firebase.initializeApp(config);
 // Reference to Firebase database
 var dbRefObject = firebase.database().ref();
 
-// console.log(database);.r
+// console.log(database);
 
 // Event listener for Submit button
 document.getElementById("submitBtn").addEventListener("click", function () {
@@ -25,18 +23,20 @@ document.getElementById("submitBtn").addEventListener("click", function () {
     // Stop default behavior of page reload
     event.preventDefault();
 
+
     // Store new train details from user input
     var name = document.getElementById("name").value.trim();
     var destination = document.getElementById("destination").value.trim();
-    var time = document.getElementById("time").value.trim();
+    var initialStart = document.getElementById("time").value.trim();
     var frequency = document.getElementById("frequency").value.trim();
 
     // Set first time and frequency to user input
-    firstTime = time;
-    tFrequency = frequency;
+    var firstTime = initialStart;
+    var tFrequency = frequency;
+    var arrival;
 
     // Get current time in military format
-    currentTime = moment().format("HH:mm");
+    var currentTime = moment().format("HH:mm");
 
     // Set firstTime 1 month back and show results in minutes
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "month");
@@ -48,26 +48,26 @@ document.getElementById("submitBtn").addEventListener("click", function () {
 
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
+    console.log("REMAINDER: " + tRemainder);
 
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
-    var nextTrain = moment().add(minsTilTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("LT"));
+    var tArrival = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(tArrival).format("LT"));
 
-
-
-
+    arrival = tArrival;
 
     train = {
         name: name,
         destination: destination,
-        time: time,
-        frequency: frequency
+        time: initialStart,
+        frequency: parseInt(frequency),
+        minutes: tMinutesTillTrain
     }
+
     dbRefObject.push(train);
 
     var tbody = document.getElementById("tbody");
@@ -77,27 +77,40 @@ document.getElementById("submitBtn").addEventListener("click", function () {
 
 dbRefObject.on("child_added", snapshot => {
     console.log("child added");
+    // var objRefKey = JSON.stringify(snapshot.key);
+    var tName = document.createTextNode(snapshot.val().name);
+    var tDestination = document.createTextNode(snapshot.val().destination);
+    var tFrequency = document.createTextNode(snapshot.val().frequency);
+    var tMinsToArrive = document.createTextNode(snapshot.val().minutes);
+    console.log("Train Name: " + tName);
+    console.log("Train Destination: " + tDestination);
+    console.log("Train Frequency: " + tMinsToArrive);
+    console.log("Minutes Away: " + tFrequency);
+    console.log(snapshot.numChildren());
+
+    var trainName = document.createElement("td").appendChild(tName);
+
+    // console.log("Child key: " + objRefKey);
+    // console.log(snapshot.val().objRefKey);
+
 
     // TODO: assign the values of the childSnapshot to the variables below
-    var name, destination, frequency, time;
+    // var sched_Name = document.createElement("td").setAttribute("id", "sched_name").innerText = tName;
+    // var sched_Dest = document.createElement("td").setAttribute("id", "sched_dest").innerText = tDestination;
+    // var sched_Freq = document.createElement("td").setAttribute("id", "sched_freq").innerText = tFrequency;
+    // var sched_Arrive = document.createElement("td").setAttribute("id", "sched_arrive").innerText = "Figure this out";
+    // var sched_Mins = document.createElement("td").setAttribute("id", "sched_mins").innerText = tMinsToArrive;
 
-    // var sched_Name = document.createElement(td).setAttribute("id", sched_name);
-    // var sched_Dest = document.createElement(td).setAttribute("id", sched_dest);
-    // var sched_Freq = document.createElement(td).setAttribute("id", sched_freq);
-    // var sched_Arrive = document.createElement(td).setAttribute("id", sched_arrive);
-    // var sched_Mins = document.createElement(td).setAttribute("id", sched_mins);
+    // var tbody = document.getElementById("tbody");
+    // var newRow = tbody.insertRow();
 
-    // var snapKey = snapshot.key;
-    // console.log(snapKey);
-    // console.log(snapshot.numChildren());
+    // newRow.appendChild(sched_Name).appendChild(sched_Dest).appendChild(sched_Freq).appendChild(sched_Arrive).appendChild(sched_Mins);
 
-    // snapshot.forEach(childSnapShot => {
-
-    //     var childKey = childSnapShot.key;
-    //     var childData = childSnapShot.val();
-
-    //     console.log("childData: " + childData);
-    // });
+    snapshot.forEach(childSnapShot => {
+        var childKey = childSnapShot.key;
+        var childData = childSnapShot.val();
+        console.log("childKey: " + childKey);
+    });
 });
 
 dbRefObject.on("value", snapshot => {
@@ -116,5 +129,3 @@ dbRefObject.on("child_removed", snapshot => {
 dbRefObject.on("child_changed", snapshot => {
     console.log("child changed");
 });
-
-
