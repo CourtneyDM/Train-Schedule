8969f79c24d0a05288e6ede84bc0c0d0239eb917
Courtney M <courtdmontgomery@gmail.com>
2018-04-18 19:02:18 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var train = {};
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBBlBKhrUTu7HO7oA0TwRV996lDLiogd4k",
+    apiKey: "",
     authDomain: "train-sched-cm.firebaseapp.com",
     databaseURL: "https://train-sched-cm.firebaseio.com",
     projectId: "train-sched-cm",
